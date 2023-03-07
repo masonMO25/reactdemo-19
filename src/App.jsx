2c732c8713871c07f8f7d606ed9f3d842344c101
masonMO25 <masonneale@gmail.com>
2023-03-06 20:35:06 -0600

    updated app with new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
+import CatCard from "./components/CatCard";
+import GroceryCard from "./components/groceryCard";
 import Header from "./components/Header";
+import NavBar from "./components/NavBar";
+import UserCard from "./components/UserCard";
+import catsData from "./data/cats";
+import groceriesData from "./data/groceries";
+import userData from "./data/users";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <NavBar />
+      <Header text="Hello 🐱" />
+
+      {catsData.map((cat) => {
+        return <CatCard cat={cat} key={cat.id} />;
+      })}
+
+      <ul>
+        {groceriesData.map((grocery) => {
+          return <GroceryCard grocery={grocery} key={grocery.id} />;
+        })}
+      </ul>
+
+      {userData.map((user) => {
+        return <UserCard user={user} key={user.id.value} />;
+      })}
+    </>
+  );
 }
 
 export default App;
+
+//* <CatCard cat={catsData[0]} />
